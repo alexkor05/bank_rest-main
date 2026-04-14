9e280835f9e93244ef3192da4903fc586ca7e5a3
Alexey Korobov <boxal@yandex.ru>
2026-04-14 13:52:56 +0300

    update: update Card entity - add new fields for card

--- src/main/java/com/example/bankcards/entity/Card.java
+++ src/main/java/com/example/bankcards/entity/Card.java
@@ -1,5 +1,18 @@
 package com.example.bankcards.entity;
 
-public class Card {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Card {
+    private Long id;
+    private String cardNumber;
+    private BigDecimal balance;
+    private LocalDate expiredDate;
 }
