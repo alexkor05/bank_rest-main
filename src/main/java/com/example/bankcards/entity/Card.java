fef14d958cd7251a31370563f79b0ce936dd5b86
Alexey Korobov <boxal@yandex.ru>
2026-04-14 13:52:56 +0300

    update: update Card entity - add new fields for card

--- src/main/java/com/example/bankcards/entity/Card.java
+++ src/main/java/com/example/bankcards/entity/Card.java
@@ -1,5 +1,27 @@
 package com.example.bankcards.entity;
 
-public class Card {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "card")
+public class Card {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String cardNumber;
+    private BigDecimal balance;
+    private LocalDate expiredDate;
+
+    @Enumerated(EnumType.STRING)
+    private Status status;
 }
