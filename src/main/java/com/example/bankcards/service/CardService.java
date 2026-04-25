fa6cd017c25550f124af9828ba5a16518aacde9c
Alexey Korobov <boxal@yandex.ru>
2026-04-26 02:33:44 +0300

    feat: add services - add UserService - add CardService

---
+++ src/main/java/com/example/bankcards/service/CardService.java
@@ -0,0 +1,25 @@
+package com.example.bankcards.service;
+
+import com.example.bankcards.dto.CardDto;
+import com.example.bankcards.dto.CreateCardRequest;
+import com.example.bankcards.entity.Card;
+import com.example.bankcards.mapper.CardMapper;
+import com.example.bankcards.repository.CardRepository;
+import com.example.bankcards.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CardService {
+    private final CardRepository cardRepository;
+    private final UserRepository userRepository;
+    private final CardMapper cardMapper;
+
+    public CardDto createCard(CreateCardRequest createCardRequest) {
+        Card card = cardMapper.toCard(createCardRequest);
+        card.setUser(userRepository.findById(createCardRequest.userId()).orElseThrow());
+        Card createdCard = cardRepository.save(card);
+        return cardMapper.toCardDto(createdCard);
+    }
+}
---
+++ src/main/java/com/example/bankcards/service/UserService.java
@@ -0,0 +1,27 @@
+package com.example.bankcards.service;
+
+import com.example.bankcards.dto.CreateUserRequest;
+import com.example.bankcards.dto.UserDto;
+import com.example.bankcards.entity.User;
+import com.example.bankcards.mapper.UserMapper;
+import com.example.bankcards.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserMapper userMapper;
+    private final UserRepository userRepository;
+
+    public UserDto findById(Long id) {
+        User user = userRepository.findByIdWithCards(id).orElseThrow();
+        return userMapper.toUserDto(user);
+    }
+
+    public UserDto createUser(CreateUserRequest createUserRequest) {
+        User user = userMapper.toUser(createUserRequest);
+        User createdUser = userRepository.save(user);
+        return userMapper.toUserDto(createdUser);
+    }
+}
