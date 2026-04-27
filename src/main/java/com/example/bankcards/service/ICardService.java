ee06ec3cadc9d26426e39845de2d8b696faae010
Alexey Korobov <boxal@yandex.ru>
2026-04-28 02:29:36 +0300

    update: update IUserService and ICardService - add update, delete methods

--- src/main/java/com/example/bankcards/service/ICardService.java
+++ src/main/java/com/example/bankcards/service/ICardService.java
@@ -2,7 +2,11 @@ package com.example.bankcards.service;
 
 import com.example.bankcards.dto.CardDto;
 import com.example.bankcards.dto.CreateCardRequest;
+import com.example.bankcards.dto.UpdateCardRequest;
 
 public interface ICardService {
     CardDto createCard(CreateCardRequest createCardRequest);
+    CardDto findById(Long id);
+    CardDto updateCard(Long id, UpdateCardRequest updateCardRequest);
+    void delete(Long id);
 }
--- src/main/java/com/example/bankcards/service/IUserService.java
+++ src/main/java/com/example/bankcards/service/IUserService.java
@@ -1,9 +1,12 @@
 package com.example.bankcards.service;
 
 import com.example.bankcards.dto.CreateUserRequest;
+import com.example.bankcards.dto.UpdateUserRequest;
 import com.example.bankcards.dto.UserDto;
 
 public interface IUserService {
     UserDto findById(Long id);
     UserDto createUser(CreateUserRequest createUserRequest);
+    UserDto updateUser(Long id, UpdateUserRequest updateUserRequest);
+    void deleteUser(Long id);
 }
