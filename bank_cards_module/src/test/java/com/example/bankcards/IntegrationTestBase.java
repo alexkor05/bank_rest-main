3984ef0257bcd207dc45159f93ff3b2d5989ae3e
Alexey Korobov <boxal@yandex.ru>
2026-06-11 01:55:54 +0300

    update(IntegrationTestBase): add redisContainer

--- bank_cards_module/src/test/java/com/example/bankcards/IntegrationTestBase.java
+++ bank_cards_module/src/test/java/com/example/bankcards/IntegrationTestBase.java
@@ -3,12 +3,16 @@ package com.example.bankcards;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlGroup;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -16,18 +20,18 @@ import org.testcontainers.containers.PostgreSQLContainer;
         @Sql(scripts = "classpath:sql/clean.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD),
         @Sql(scripts = "classpath:sql/data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 })
+@Testcontainers
 public abstract class IntegrationTestBase {
-    private static final PostgreSQLContainer<?> container = new PostgreSQLContainer<>("postgres:16.12");
 
-    @BeforeAll
-    static void runContainer() {
-        container.start();
-    }
 
-    @DynamicPropertySource
-    static void postgresProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", container::getJdbcUrl);
-    }
+    @Container
+    @ServiceConnection
+    static final PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:16.12");
+
+    @Container
+    @ServiceConnection
+    static final GenericContainer<?> redisContainer = new GenericContainer<>("redis:7.4-alpine")
+            .withExposedPorts(6379);
 
 
 }
