068a1dd34ae05f6e2020bede497805cd0b849c99
Alexey Korobov <boxal@yandex.ru>
2026-08-27 19:02:23 +0300

    fix: fix bankEvents property

--- bank_cards_module/src/main/java/com/example/bankcards/outbox/dto/BankEvent.java
+++ bank_cards_module/src/main/java/com/example/bankcards/outbox/dto/BankEvent.java
@@ -2,12 +2,6 @@ package com.example.bankcards.outbox.dto;
 
 import com.example.bankcards.dto.EventType;
 import com.example.bankcards.outbox.entity.AggregateType;
-import com.example.bankcards.outbox.entity.EventStatus;
-import jakarta.persistence.Column;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import org.hibernate.annotations.JdbcTypeCode;
-import org.hibernate.type.SqlTypes;
 import tools.jackson.databind.JsonNode;
 
 import java.time.LocalDateTime;
